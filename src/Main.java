3fc8fb5d59edc94ef1b04dd796e64bb905801256
AhmedYu <abyan-adan@hotmail.com>
2025-10-21 12:10:26 -0500

    initial commint

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+git remote add origin https://github.com/AhmedYussuf1/ProgrammingAssignemnt3.git
+git branch -M main
+git push -u origin mainpublic class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
