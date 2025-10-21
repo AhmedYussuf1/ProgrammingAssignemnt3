7211044f1f1a6aadd99419d2a3f7daa91f240452
AhmedYu <abyan-adan@hotmail.com>
2025-10-21 14:11:48 -0500

    turned RecordStudio into abstract class

--- src/RecordStudioOrderedListDriver.java
+++ src/RecordStudioOrderedListDriver.java
@@ -4,5 +4,6 @@
         RecordStudio r2 = new RecordStudio("VT" , 33);
          System.out.println(r.compareTo(r2));
          System.out.println(r.equals(r2));
-     }
+         SocialMediaContentStudio s = new SocialMediaContentStudio("wllness", 32, 4);
+         System.out.println(s.toString());     }
  }
